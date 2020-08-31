842048a5a6dd1e354d344db48f2bb4d179da321e
Yesman-coder <yesmanshiva@gmail.com>
2020-08-31 17:21:14 +0000

    :snake: first dia and classes created

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,60 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
+    user_name = Column(String, primary_key=True)
+    name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250), nullable=False)
+    password = Column(String(250))
+    profile_pic = Column(String(250))
+    followers = Column(Integer())
+    followed = Column(Integer())
+    notifications = Column(Integer())
+    posts = relationship("Post", backref="author")
+    inbox = relationship("Inbox", backref="author")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+    __tablename__= 'post'
+    post_id = Column(Integer, primary_key=True)
+    user_name = Column(String, ForeignKey('users.user_name'), nullable=False)
+    content = Column(String(250))
+    date = Column(DateTime())
+    likes = Column(Integer())
+    comments = Column(String(250))
+    saved = Column()
+
+class Saved(Base):
+    __tablename__ = 'saved'
+    saved_id = Column(Integer, primary_key=True)
+    user_name = Column(String, ForeignKey('users.user_name'))
+    post_id = Column(String, ForeignKey('post.post_id'))
+    posts = relationship("Post")
+
+class Feed(Base):
+    __tablename__ = 'feed'
+    feed_id = Column(Integer, primary_key=True)
+    user_name = Column(String, ForeignKey('users.user_name'))
+    post_id = Column(String, ForeignKey('post.post_id'))
+    date = Column(DateTime())
+    posts = relationship("Post")
+
+class Stories(Base):
+    __tablename__ = 'stories'
+    stories_id = Column(Integer, primary_key=True)
+    user_name = Column(String, ForeignKey('users.user_name'))
+    post_id = Column(String, ForeignKey('post.post_id'))
+    posts = relationship("Post")
+
+class Inbox(Base):
+    __tablename__ = 'inbox'
+    inbox_id = Column(Integer, primary_key=True)
+    user_name = Column(Integer, ForeignKey('users.user_name'), nullable=False)
+    new_messages = Column(String(200))
+    old_messages = Column(String(200))
+    
+
 
     def to_dict(self):
         return {}
